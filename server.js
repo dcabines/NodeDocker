10d2b3a24b3f8eab17dc38f042a2f40858f8c5ca
David Cabiness <dcabiness@werktek.com>
2016-12-07 16:07:40 -0500

    more urls

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var port = 80;
 var express = require('express');
+var cookie = require('cookie');
+var logger = require('./logger');
 var app = express();
+
+app.use(logger);
 
 app.get('/', function (req, res) {
   res.send('Hello World\n');
@@ -8,6 +12,15 @@ app.get('/', function (req, res) {
 
 app.get('/headers', function (req, res) {
   res.json(req.headers);
+});
+
+app.get('/cookies', function (req, res) {
+  var cookies = cookie.parse(req.headers.cookie)
+  res.json(cookies);
+});
+
+app.get('/address', function (req, res) {
+  res.json(req.connection.address());
 });
 
 app.listen(port);
